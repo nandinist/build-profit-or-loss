f66dd4cf4ac0c816667ebe17e76526fc1d22e3c6
Nandinist <nandini160913@gmail.com>
2022-10-18 14:04:20 +0530

    style : changed font

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
 var initialPrice = document.querySelector("#initial-price");
 var stocksQuantity = document.querySelector("#stocks-quantity");
 var currentPrice = document.querySelector("#current-price");
-var submitBtn = document.querySelector("#submit-btn");
+var submit = document.querySelector("#submit-btn");
 var outputBox = document.querySelector("#output-box");
 
-// Ex-06
-submitBtn.addEventListener("click", submitHandler);
+submit.addEventListener("click", submitHandler);
 
 function submitHandler() {
-  var ip = Number(initialPrice.value);
-  var qty = Number(stocksQuantity.value);
+  var intialValue = Number(initialPrice.value);
+  var quantityOfStock = Number(stocksQuantity.value);
   var curr = Number(currentPrice.value);
 
-  calculateProfitAndLoss(ip, qty, curr);
+  ProfitAndLoss(intialValue, quantityOfStock, curr);
 }
 
-// Ex-05
-function calculateProfitAndLoss(initial, quantity, current) {
-  if (initial > current) {
-    var loss = (initial - current) * quantity;
-    var lossPercentage = (loss / initial) * 100;
+function ProfitAndLoss(initialVal, quantity, currPrice) {
+  if (initialVal > currPrice) {
+    var loss = (initialVal - currPrice) * quantity;
+    var lossPercent = (loss / initial) * 100;
 
-    showOutput(
-      `Hey, the loss is ${loss} and the percent is ${lossPercentage}%`
-    );
-  } else if (current > initial) {
-    var profit = (current - initial) * quantity;
-    var profitPercentage = (profit / initial) * 100;
+    showOutput(`Loss: ${loss} and Loss Percent: ${lossPercent}%`);
+  } else if (currPrice > initial) {
+    var profit = (currPrice - initialVal) * quantity;
+    var profitPercent = (profit / initialVal) * 100;
 
-    showOutput(
-      `Hey, the profit is ${profit} and the percent is ${profitPercentage}%`
-    );
+    showOutput(`Profit: ${profit} and Gain Percent: ${profitPercent}%`);
   } else {
     showOutput(`No pain no gain and no gain no pain`);
   }
